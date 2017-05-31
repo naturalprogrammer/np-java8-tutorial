d969f7fe821b1683f724b3c565d7e3b1cc9a1ca2
Sanjay Patel <skpatel20@gmail.com>
2017-05-31 14:35:45 +0530

    Passing behavior

--- src/main/java/com/naturalprogrammmer/java8tutorial/App.java
+++ src/main/java/com/naturalprogrammmer/java8tutorial/App.java
@@ -8,6 +8,23 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Hello World!");
+        System.out.println( process("Hello World!", new Processor() {
+
+			public String process(String str) {
+				
+				return str.toLowerCase();
+			}
+        	
+        }) );
     }
+
+	private static String process(String str, Processor processor) {
+		
+		return processor.process(str);
+	}
+}
+
+interface Processor {
+	
+	String process(String str);
 }
